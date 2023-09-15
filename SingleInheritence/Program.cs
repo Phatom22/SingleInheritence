7d5db5b1f89be3c595a45cfb8e7ab75a2fc4a1f4
nomvula <nzshabangu95@gmail.com>
2023-09-15 04:17:41 +0200

    Add project files.

---
+++ SingleInheritence/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleInheritence
+{
+  
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Accountcreditinfo myObj1 = new Accountcreditinfo();
+            Console.WriteLine(myObj1.Credit());
+            Console.ReadLine();
+        }
+        
+
+    }
+    public class Accountcreditinfo //base class
+    {
+        public string Credit()
+        {
+            return "balance is credited";
+        }
+    }
+    public class debitinfo : Accountcreditinfo //derived class
+    {
+        public string debit()
+        {
+            Credit();                       ////derived class method
+            return "balance is debited";
+        }
+    }
+
+}
